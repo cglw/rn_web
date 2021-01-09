ae01698d9fa9ac2a6cd60c91ecda7b5e4de2039d
肖文佳 <xiaowenjia@baijiayun.com>
2021-01-09 09:40:55 +0800

    eslint添加注释说明

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,11 @@ module.exports = {
   plugins: ['@typescript-eslint'],
   // 0 代表关闭  1 代表警告  2 代表开启
   rules: {
-    'no-extend-native': 0,
-    'no-undef': 0,
+    'no-extend-native': 0, //是否允许扩展native对象
+    'no-undef': 0, //是否能有未定义的变量
     'dot-notation': 0,
     'react-native/no-inline-styles': 0,
     'prettier/prettier': 2,
+    'no-var': 2, //是否能用var，用let和const代替
   },
 };
