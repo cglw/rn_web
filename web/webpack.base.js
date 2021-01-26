b8860464d413774f83b57976e3cd5de5e8b505de
肖文佳 <xiaowenjia@baijiayun.com>
2021-01-26 16:48:30 +0800

    <新增>(antd-mobile): 为web端添加 antd-mobile 支持

--- web/webpack.base.js
+++ web/webpack.base.js
@@ -46,6 +46,10 @@ module.exports = {
         ],
         exclude: /node_modules/,
       },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
   plugins: [
